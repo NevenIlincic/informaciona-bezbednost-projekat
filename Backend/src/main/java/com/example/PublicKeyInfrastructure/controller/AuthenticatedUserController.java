4bdb58a126ec8fd34da2fc3cc3778bef5063aa8a
Neven Ilincic <nevenilincic@gmail.com>
2025-10-15 23:16:03 +0200

    [ADD] Simple user creating

--- Backend/src/main/java/com/example/PublicKeyInfrastructure/controller/AuthenticatedUserController.java
+++ Backend/src/main/java/com/example/PublicKeyInfrastructure/controller/AuthenticatedUserController.java
@@ -1,11 +1,24 @@
 package com.example.PublicKeyInfrastructure.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.PublicKeyInfrastructure.dto.authenticatedUser.CreateAuthenticatedUserDTO;
+import com.example.PublicKeyInfrastructure.dto.organization.RegistrationOrganizationDTO;
+import com.example.PublicKeyInfrastructure.service.AuthenticatedUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin
 public class AuthenticatedUserController {
+
+    @Autowired
+    private AuthenticatedUserService authenticatedUserService;
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createUser(@RequestBody CreateAuthenticatedUserDTO userToCreate) {
+        authenticatedUserService.createUser(userToCreate);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
--- Backend/src/main/java/com/example/PublicKeyInfrastructure/service/AuthenticatedUserService.java
+++ Backend/src/main/java/com/example/PublicKeyInfrastructure/service/AuthenticatedUserService.java
@@ -1,5 +1,9 @@
 package com.example.PublicKeyInfrastructure.service;
 
+import com.example.PublicKeyInfrastructure.dto.authenticatedUser.CreateAuthenticatedUserDTO;
+import com.example.PublicKeyInfrastructure.model.AuthenticatedUser;
+import com.example.PublicKeyInfrastructure.model.Organization;
+import com.example.PublicKeyInfrastructure.model.Role;
 import com.example.PublicKeyInfrastructure.repository.AuthenticatedUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +12,21 @@ import org.springframework.stereotype.Service;
 public class AuthenticatedUserService {
     @Autowired
     private AuthenticatedUserRepository authenticatedUserRepository;
+    @Autowired
+    private OrganizationService organizationService;
+
+    public AuthenticatedUser createUser(CreateAuthenticatedUserDTO userToCreate) {
+        Organization foundOrganization = organizationService.findOrganizationById(userToCreate.getOrganization().getId());
+        AuthenticatedUser userToSave = new AuthenticatedUser();
+        userToSave.setEmail(userToCreate.getEmail());
+        userToSave.setFirstName(userToCreate.getFirstName());
+        userToSave.setLastName(userToCreate.getLastName());
+        userToSave.setRole(Role.REGULAR_USER);
+        userToSave.setOrganization(foundOrganization);
+        userToSave.setPassword(userToCreate.getPassword());
+        userToSave.setIsActive(true);
+
+        return authenticatedUserRepository.save(userToSave);
+
+    }
 }
