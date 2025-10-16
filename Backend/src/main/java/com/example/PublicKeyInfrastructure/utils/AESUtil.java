e5b19d0fd0c8592beaf4d7db0798c1d8742d6cbd
Neven Ilincic <nevenilincic@gmail.com>
2025-10-16 21:39:50 +0200

    [ADD] Putting encrypted activation token in URL

--- Backend/src/main/java/com/example/PublicKeyInfrastructure/utils/AESUtil.java
+++ Backend/src/main/java/com/example/PublicKeyInfrastructure/utils/AESUtil.java
@@ -9,6 +9,7 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Base64;
 
 @Component
@@ -17,16 +18,20 @@ public class AESUtil {
     public String encrypt(String message){
         try {
             byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
-            String secretKey = System.getenv("SECRET_KEY");
-            byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
-            SecretKey secretKeyAES = new SecretKeySpec(keyBytes, "AES");
-            IvParameterSpec iv = generateIV();
-            Cipher aesCipherEnc = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            aesCipherEnc.init(Cipher.ENCRYPT_MODE, secretKeyAES, iv);
+            SecretKey secretKey = getSecretKey();
+            byte[] ivBytes = generateIV();
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(ivBytes);
 
+            Cipher aesCipherEnc = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            aesCipherEnc.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec);
             //sifrovanje
             byte[] ciphertext = aesCipherEnc.doFinal(messageBytes);
-            String encodedCiphertext = Base64.getEncoder().encodeToString(ciphertext);
+
+            byte[] combined = new byte[ivBytes.length + ciphertext.length];
+            System.arraycopy(ivBytes, 0, combined, 0, ivBytes.length);
+            System.arraycopy(ciphertext, 0, combined, ivBytes.length, ciphertext.length);
+
+            String encodedCiphertext = Base64.getUrlEncoder().withoutPadding().encodeToString(combined);
 
             return encodedCiphertext;
         }catch (Exception e){
@@ -35,13 +40,39 @@ public class AESUtil {
         return null;
     }
 
-    private IvParameterSpec generateIV() {
-        byte []iv = new byte[16];
+    public String decrypt(String encryptedBase64){
+        try {
+            byte[] combined = Base64.getUrlDecoder().decode(encryptedBase64);
 
+            // Prvih 16 bajtova je IV
+            byte[] iv = Arrays.copyOfRange(combined, 0, 16);
+            byte[] encrypted = Arrays.copyOfRange(combined, 16, combined.length);
+            System.out.println(combined.length);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            SecretKey secretKey = getSecretKey();
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+            //dekriptovanje
+            byte[] decrypted = cipher.doFinal(encrypted);
+            return new String(decrypted, StandardCharsets.UTF_8);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private SecretKey getSecretKey(){
+        String secretKey = System.getenv("SECRET_KEY");
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        SecretKey secretKeyAES = new SecretKeySpec(keyBytes, "AES");
+        return secretKeyAES;
+    }
+
+    private byte[] generateIV() {
+        byte []iv = new byte[16];
         SecureRandom secureRandom = new SecureRandom();
         secureRandom.nextBytes(iv);
-
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
-        return ivParameterSpec;
+        return iv;
     }
 }
