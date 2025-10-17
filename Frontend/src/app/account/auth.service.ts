411b33212d76544cd654f967879cc68043ae490d
Neven Ilincic <nevenilincic@gmail.com>
2025-10-17 23:20:20 +0200

    [ADD] Navbar

--- Frontend/src/app/account/auth.service.ts
+++ Frontend/src/app/account/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 export interface TokenResponse {
   accessToken: string;
@@ -18,14 +19,22 @@ export interface LoginDTO {
 })
 export class AuthService {
   private apiUrl = 'https://localhost:8080/api/auth'; // ako koristiš HTTPS backend
+  
+  user$ = new BehaviorSubject<string>(this.getRole());
+  userState = this.user$.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.user$.next(this.getRole());
+  }
+
+
 
   login(loginData: LoginDTO): Observable<TokenResponse> {
     return this.http.post<TokenResponse>(`${this.apiUrl}/login`, loginData).pipe(
       tap((response: TokenResponse) => {
         localStorage.setItem('accessToken', response.accessToken);
         localStorage.setItem('refreshToken', response.refreshToken);
+        this.user$.next(this.setRole(response.accessToken));
       }),
       catchError(err => {
         console.error('Login error:', err);
@@ -37,9 +46,25 @@ export class AuthService {
   logout(): void {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
+    this.user$.next("");
   }
 
   isLoggedIn(): boolean {
     return !!localStorage.getItem('accessToken');
+  }
+
+  
+  getRole(): any {
+    if (this.isLoggedIn()) {
+      const accessToken: any = localStorage.getItem('accessToken'); // Ako je prijavljen, token se vec nalazi u localStorage
+      const helper = new JwtHelperService();
+      return helper.decodeToken(accessToken).role; /// Preuzimanje uloge iz tokena
+    }
+    return null;
+  }
+
+  setRole(accessToken: string): string {
+    const helper = new JwtHelperService();
+    return helper.decodeToken(accessToken).role;
   }
 }
---
+++ Frontend/src/app/certificate-form-page/certificate-form-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificate-form-page',
+  imports: [],
+  templateUrl: './certificate-form-page.html',
+  styleUrl: './certificate-form-page.css'
+})
+export class CertificateFormPage {
+
+}
